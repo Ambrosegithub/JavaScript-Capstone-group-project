2bac811665cda2401d172b83179d61d202bd631a
Isigba Ambrose <ambrisexyz@gmail.com>
2022-08-02 10:51:38 +0200

    choose an External API for our project

---
+++ src/modules/service.js
@@ -0,0 +1,3 @@
+const showsBaseUrl = 'https://api.tvmaze.com/search/shows?q=girls';
+const likesBaseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/dJUZAxNGSAzuOwOz1Flo/likes/';
+const commentsBaseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/dJUZAxNGSAzuOwOz1Flo/comments/';
