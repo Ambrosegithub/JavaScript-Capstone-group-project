a529f514e13befcd738111602003ebc70b0bc394
Custodio Serafim Langa <langacustodio@gmail.com>
2022-08-03 11:42:30 +0200

    Add popup comment with selected item details

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import './index.css';
 /* eslint-disable import/prefer-default-export */
 import { Display } from './modules/display.js';
-import { addLikes, getAllLikes } from './modules/service.js';
+import { addLikes, getAllLikes, getAllMovies } from './modules/service.js';
 
 await Display();
 
+const shows = await getAllMovies();
+const likes = await getAllLikes();
+const modal = document.querySelector('.modal');
+const closeButton = document.querySelector('.close-button');
+const image = document.getElementById('img-detail');
+const btnModalComments = document.querySelectorAll('#modal-comments');
+const total = document.querySelectorAll('.likeId');
+const btnLikes = document.querySelectorAll('#btnLikes');
+
+// START LIKES SECTION
+
 export const showLikes = async (showId) => {
   let totalLikes = '';
-  const likes = await getAllLikes();
 
   const like = likes.filter((l) => l.item_id === showId);
   if (like[0]) {
@@ -15,10 +25,6 @@ export const showLikes = async (showId) => {
   }
   return totalLikes;
 };
-
-const total = document.querySelectorAll('.likeId');
-
-const btnLikes = document.querySelectorAll('#btnLikes');
 
 btnLikes.forEach((button) => {
   button.addEventListener('click', (event) => {
@@ -32,3 +38,28 @@ btnLikes.forEach((button) => {
     });
   });
 });
+
+// END LIKES SECTION
+
+// START COMMENT MODAL SECTION
+
+const toggleModal = (showId) => {
+  const show = shows.filter((s) => s.show.id === showId);
+
+  if (show.length > 0) {
+    image.src = show[0].show.image.medium;
+  }
+
+  modal.classList.toggle('show-modal');
+};
+
+btnModalComments.forEach((Modalcomment) => {
+  Modalcomment.addEventListener('click', (event) => {
+    event.preventDefault();
+    const id = parseInt(event.target.getAttribute('data-showid'), 10);
+    toggleModal(id);
+  });
+});
+
+closeButton.addEventListener('click', toggleModal);
+// END COMMENT MODAL SECTION
